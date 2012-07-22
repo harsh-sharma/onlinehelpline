d5ad7425b2a53f196423aab9094990618a99ed9b
Harsh Sharma <er.harsh.sharma@gmail.com>
2012-07-23 01:38:27 +0200

    Update public/includes/constants.php

--- public/includes/constants.php
+++ public/includes/constants.php
@@ -1,6 +1,6 @@
 <?php
 	define("DB_SERVER","a.db.shared.orchestra.io");
-	define("DB_USER","user_2cb49c48");
-	define("DB_Pass","nvgZNB0jnq%nn0");
-	define("DB_NAME","db_2cb49c48");
+	define("DB_USER","user_bfa761d8");
+	define("DB_Pass","V,wTqCuFF2hUHp");
+	define("DB_NAME","db_bfa761d8");
 ?>
